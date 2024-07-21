a96c00e1f0dd8b6c7c92682cb7847a1a67b10358
glauco.r3 <glauco.r3@gmail.com>
2024-07-21 17:18:54 -0300

    Navigation

---
+++ webapp/controller/Create.controller.js
@@ -0,0 +1,147 @@
+sap.ui.define(
+    [
+        "./BaseController",
+        "sap/ui/model/json/JSONModel",
+        "sap/ui/core/routing/History",
+        "sap/m/MessageToast",
+        "sap/ui/core/UIComponent",
+    ],
+    function (BaseController, JSONModel, History, MessageToast, UIComponent) {
+        "use strict";
+        return BaseController.extend("fresstyledozerov02.controller.Create", {
+            onInit: function () {
+
+                var oRouter = sap.ui.core.UIComponent.getRouterFor(this);
+                oRouter
+                    .getRoute("create")
+                    .attachPatternMatched(this._onCreateMatched, this);
+
+                //MODEL p/ iniciar valors
+                var oViewModel = new JSONModel({
+                    Codigo: 0,
+                    Descricao: "",
+                    Kwmeng: "0.000",
+                    Meins: "KG",
+                    Netpr: "0.00",
+                    Waerk: "BRL",
+                });
+                this.getView().setModel(oViewModel, "view");
+            },
+
+            _onCreateMatched: function (oEvent) {
+                var m = this.getView().getModel();
+                m.metadataLoaded().then(
+                    function () {
+                        var oContext = m.createEntry("/Z270CADPRODUTOSSet", {
+                            properties: {
+                                Descricao: "",
+                                Kwmeng: "0.00",
+                                Meins: "",
+                                Netpr: "0.00",
+                                Waerk: "",
+                            },
+                        });
+                        this.getView().bindElement({
+                            path: oContext.getPath(),
+                            expand: "view",
+                            //model: "view",
+                        });
+                    }.bind(this)
+                );
+            },
+
+            onNavBack: function () {
+                var oHistory = History.getInstance();
+                var sPreviousHash = oHistory.getPreviousHash();
+
+                if (sPreviousHash !== undefined) {
+                    window.history.go(-1);
+                } else {
+                    var oRouter = UIComponent.getRouterFor(this);
+                    oRouter.navTo("worklist", {}, true);
+                }
+            },
+
+            parseFloatExternoBrasileiro(string) { },
+
+            onGravar: function () {
+                //Limpar mensagens antigas
+                sap.ui.getCore().getMessageManager().removeAllMessages();
+
+                var oModel = this.getView().getModel();
+
+                var dados = {
+                    // Codigo:     this.byId("inpCodigo").getValue(),
+                    Descricao: this.byId("inpDescricao").getValue(),
+                    Kwmeng: this.byId("inpKwmeng").getValue(),
+                    Meins: this.byId("inpMeins").getValue(),
+                    Netpr: this.byId("inpNetpr").getValue(),
+                    Waerk: this.byId("inpWaerk").getValue(),
+                };
+
+                oModel.create("/Z270CADPRODUTOSSet", dados, {
+                    success: function (oDados, response) {
+                        //var lv_message = JSON.parse(response.headers["sap-message"]);
+                        //this.getView().setBusy(false);
+                        var lv_message = JSON.parse(response.headers["sap-message"]);
+                        sap.m.MessageToast.show(
+                            "Produto " + oDados.Codigo + " criado com sucesso !", {
+                                duration: 7000,                  // default
+                                width: "15em",                   // default
+                                my: "center bottom",             // default
+                                at: "center bottom",             // default
+                                of: window,                      // default
+                                offset: "0 0",                   // default
+                                collision: "fit fit",            // default
+                                onClose: null,                   // default
+                                autoClose: true,                 // default
+                                animationTimingFunction: "ease", // default
+                                animationDuration: 1000,         // default
+                                closeOnBrowserNavigation: true   // default);
+                            });
+                        //var mensagem = JSON.parse(resposta.headers["sap-message"]);
+                        // teste 2
+                        // this.getRouter().navTo("object", {
+                        //     objectId: dados.Codigo,
+                        // });
+
+                        oModel.refresh();
+                        //teste 1
+                        this.onNavBack();
+
+                        // teste 3
+                        //this.onNavBack("object", parseInt(dados.Codigo) );
+                    }.bind(this),
+                    error: function (Error) {
+                        var lv_mensagem = JSON.parse(Error.responseText).error.message
+                            .value;
+                        MessageToast.show(lv_mensagem + dados.Descricao, {
+                            duration: 6000,                  // default
+                            width: "15em",                   // default
+                            my: "center bottom",             // default
+                            at: "center bottom",             // default
+                            of: window,                      // default
+                            offset: "0 0",                   // default
+                            collision: "fit fit",            // default
+                            onClose: null,                   // default
+                            autoClose: true,                 // default
+                            animationTimingFunction: "ease", // default
+                            animationDuration: 1000,         // default
+                            closeOnBrowserNavigation: true   // default);
+                        });
+                        this.onNavBack();
+                        // MessageToast.show("Aconteceu um erro.");
+                        // console.error(oData);
+                        //this.getView().setBusy(false);
+                        // this.onNavBack();
+                    }.bind(this),
+                });
+            },
+
+            onCancelar: function (oEvent) {
+                this.onNavBack();
+            },
+        }
+        );
+    }
+);
